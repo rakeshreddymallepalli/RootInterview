9083f73c397366685e3e639b03b50546b1d7162f
Rakesh <admin@rakeshreddy27.com>
2019-03-16 11:29:58 -0400

    First commit - working project
    First commit - working project

---
+++ RootInt/src/com/root/inter/Driver.java
@@ -0,0 +1,20 @@
+package com.root.inter;
+
+
+public class Driver {
+	
+	private String name;
+	
+	public Driver()
+	{	  
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+		
+}
